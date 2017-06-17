70dcc2cfb04dda438e1521ba3e3f0c1b71ea9fae
Nikhil <nikhil@01HW1051420.local>
2017-06-16 19:40:03 -0700

    Added template for Home Page and charts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'page-home',
@@ -10,5 +11,67 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
+
+  ionViewDidLoad() {
+    let ctx: CanvasRenderingContext2D = (<HTMLCanvasElement>(document.getElementById("myChart"))).getContext('2d');
+
+    let myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
+
+
+    let ctx2: CanvasRenderingContext2D = (<HTMLCanvasElement>(document.getElementById("myChart2"))).getContext('2d');
+
+    let data2 = {
+      datasets: [{
+        data: [10, 20, 30]
+      }],
+
+      labels: [
+        'Red',
+        'Yellow',
+        'Blue'
+      ]
+    };
+
+    let myDoughnutChart = new Chart(ctx2, {
+      type: 'doughnut',
+      data: data2
+    });
+  }
+
 
 }
