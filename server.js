da7cb6413767812a44cfc7fbdc32b7f726f65b60
Shobhit Chittora <shobhitchittora@01HW1051654.local>
2017-06-24 21:23:14 +0530

    header icon

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 app = express();
-app.use(express.static(__dirname + '/www'));
-app.set('port', process.env.PORT || 5000);
+app.use(express.static('www'));
+app.set('port', process.env.PORT || 8080);
 app.listen(app.get('port'), () => {
     console.log('EXPRESS SERVER LISTENING ON PORT ' + app.get('port'));
 });
