f71b460141d925c7128f27f85b2b2cd21550731f
Shobhit Chittora <shobhitchittora@01HW1051654.local>
2017-06-24 20:43:13 +0530

    node server changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 app = express();
-app.use(express.static('www'));
+app.use(express.static(__dirname + '/www'));
 app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'), () => {
     console.log('EXPRESS SERVER LISTENING ON PORT ' + app.get('port'));
